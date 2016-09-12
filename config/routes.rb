8b356998446364f8018b901069b1cf1d5c43b123
rsilva77 <ramon.silva.ismart@gmail.com>
2016-09-12 15:51:12 -0300

    Admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # auth routes
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
+
+  devise_scope :admin do
+  	get 'admin', to: 'devise/sessions#new'
+  end
   devise_for :admins
 
+  # other routes
   root to: 'public#index'
+  get 'admin_panel', to: 'admin#index', as: :admin_root
 end
